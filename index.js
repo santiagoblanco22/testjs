978a2e19c0e9965676a67a05ad0a16a94fd39bb5
santiagoblanco22 <109559178+santiagoblanco22@users.noreply.github.com>
2022-10-10 17:50:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("Hola")
+const scrollToBottom = (id) => {
+    const element = document.getElementByClassName(id);
+    element.scrollTop = element.scrollHeight;
+}
+
+let selector = $('body > div.main-page.bubble-element.Page.bubble-r-container.fixed > div.bubble-element.Group.bubble-r-container.fixed > div.bubble-element.RepeatingGroup.bubble-rg')
+console.log(selector)
+
+
+scrollToBottom('bubble-element')
