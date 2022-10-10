4f5bb5e4423a6723c87d0a06ddf2468790239263
santiagoblanco22 <109559178+santiagoblanco22@users.noreply.github.com>
2022-10-10 17:55:10 -0500

    Update and rename index1.js to index2.js

--- index2.js
+++ index2.js
@@ -8,4 +8,9 @@ let selector = $('body > div.main-page.bubble-element.Page.bubble-r-container.fi
 console.log(selector)
 
 
+setInterval(() => {
+    $('body > div.main-page.bubble-element.Page.bubble-r-container.fixed > div.bubble-element.Group.bubble-r-container.fixed > div.bubble-element.RepeatingGroup.bubble-rg')[0].scrollTop = 3000
+}, 3000)
+
+
 scrollToBottom('bubble-element')
