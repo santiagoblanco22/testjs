252aa95313a5ed2e69d9e776027fab9e6c314d25
santiagoblanco22 <109559178+santiagoblanco22@users.noreply.github.com>
2022-10-11 09:49:55 -0500

    Update and rename index8.js to index9.js

--- index9.js
+++ index9.js
@@ -3,12 +3,12 @@ const scrollToBottom = () => {
     const element1 = document.getElementsByClassName('RepeatingGroup')[1];
     const element = document.getElementsByClassName('RepeatingGroup')[0];
     console.log(element1)
-    element1.scrollTop = element1.scrollHeight + 500;
+    element1.scrollTop = element1.scrollHeight
 }
 
-setTimeout(() => {
+setInterval(() => {
     scrollToBottom()           
-}, 5000)
+}, 1000)
 
 $('body > div.main-page.bubble-element.Page.bubble-r-container.fixed > div.bubble-element.Group.bubble-r-container.fixed > div:nth-child(3) > div').on('click', function() { 
   console.log("click")
