9e1979cf50392897695973813f356a2fc0da2cb6
santiagoblanco22 <109559178+santiagoblanco22@users.noreply.github.com>
2022-10-11 09:45:26 -0500

    Update and rename index5.js to index6.js

---
+++ index6.js
@@ -0,0 +1,22 @@
+console.log("Hola1224")
+const scrollToBottom = () => {
+    const element1 = document.getElementsByClassName('RepeatingGroup')[1];
+    const element = document.getElementsByClassName('RepeatingGroup')[0];
+    console.log(element)
+    element.scrollTop = element.scrollHeight;
+}
+
+setTimeout(() => {
+    scrollToBottom()           
+}, 5000)
+
+$('body > div.main-page.bubble-element.Page.bubble-r-container.fixed > div.bubble-element.Group.bubble-r-container.fixed > div:nth-child(3) > div').on('click', function() { 
+  console.log("click")
+  scrollToBottom() 
+})
+
+scrollToBottom()           
+           
+
+
+
